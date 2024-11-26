608a3ea2b03505a2a45dc565d6df6d3889418fdf
Shamsiddin Kamoliddinov <160200088+bekfurr@users.noreply.github.com>
2024-11-26 09:17:04 +0500

    Add files via upload

---
+++ latin_crylic.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+
+latin_cyrillic = {
+     'a': 'а', 'b': 'б', 'd': 'д', 'e': 'е', 'f': 'ф',
+     'g': 'г', 'h': 'ҳ', 'i': 'и', 'j': 'ж', 'k': 'к',
+     'l': 'л', 'm': 'м', 'n': 'н', 'o': 'о', 'p': 'п',
+     'q': 'қ', 'r': 'р', 's': 'с', 't': 'т', 'u': 'у',
+     'v': 'в', 'x': 'х', 'y': 'й', 'z': 'з',
+     'A': 'А', 'B': 'Б', 'D': 'Д', 'E': 'Е', 'F': 'Ф',
+     'G': 'Г', 'H': 'Ҳ', 'I': 'И', 'J': 'Ж', 'K': 'К',
+     'L': 'Л', 'M': 'М', 'N': 'Н', 'O': 'О', 'P': 'П',
+     'Q': 'Қ', 'R': 'Р', 'S': 'С', 'T': 'Т', 'U': 'У',
+     'V': 'В', 'X': 'Х', 'Y': 'Й', 'Z': 'З'
+}
+
+latin_cyrillic2 = {
+    'ch':'ч', 'sh':'ш', 'o\'':'ў', 'g\'':'ғ',
+    'yu':'ю', 'ya':'я', ' e':' э', 'yo':'ё',
+    'Ch':'Ч', 'Sh':'Ш', 'O\'':'Ў', 'G\'':'Ғ',
+    'Yu':'Ю', 'Ya':'Я', ' E':' Э', 'Yo':'Ё'
+}
+
+def translate_text():
+    s = input_text.get("1.0", tk.END).strip()
+    a = ""
+    n = len(s)
+    i = 0
+
+    while i < n:
+        if i + 2 <= n and s[i:i+2] in latin_cyrillic2:
+            a += latin_cyrillic2[s[i:i+2]]
+            i += 2
+        else:
+            if s[i] in latin_cyrillic:
+                a += latin_cyrillic[s[i]]
+            else:
+                a += s[i]
+            i += 1
+
+    chiqar_text.delete("1.0", tk.END)
+    chiqar_text.insert(tk.END, a)
+
+
+oyna = tk.Tk()
+oyna.title("Lotin Krill o^giruvchisi")
+
+
+input_label = tk.Label(oyna, text="Textni kiriting Lotinda:")
+input_label.pack()
+input_text = tk.Text(oyna, height=10, width=50)
+input_text.pack()
+
+
+translate_button = tk.Button(oyna, text="Tarjima", command=translate_text)
+translate_button.pack()
+chiqar_label = tk.Label(oyna, text="Krill text:")
+chiqar_label.pack()
+chiqar_text = tk.Text(oyna, height=10, width=50)
+chiqar_text.pack()
+
+
+oyna.mainloop()
+
